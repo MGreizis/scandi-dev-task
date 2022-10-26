ff4d450e060b7981f744177157730736bcb48fac
Martins Greizis <grei0003@hz.nl>
2022-10-26 15:01:01 +0200

    created controllers for both pages

---
+++ app/controllers/AddProduct.php
@@ -0,0 +1,9 @@
+<?php
+
+class AddProduct
+{
+    public function index()
+    {
+        echo 'Hello from the index action in the AddProduct controller!';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,26 +1,30 @@
 <?php
 
+require '../app/controllers/Home.php';
+require '../app/controllers/AddProduct.php';
 require '../core/Router.php';
 
 $router = new Router();
 
 $router->add('',['controller' => 'Home', 'action' => 'index']);
-$router->add('addproduct',['controller' => 'Product', 'action' => 'index']);
+// $router->add('addproduct',['controller' => 'Product', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
 // Display the routing table
-echo '<pre>';
+// echo '<pre>';
 // var_dump($router->getRoutes());
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-echo '</pre>';
+// echo htmlspecialchars(print_r($router->getRoutes(), true));
+// echo '</pre>';
 
-$url = $_SERVER['QUERY_STRING'];
+// $url = $_SERVER['QUERY_STRING'];
 
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+// if ($router->match($url)) {
+//     echo '<pre>';
+//     var_dump($router->getParams());
+//     echo '</pre>';
+// } else {
+//     echo "No route found for URL '$url'";
+// }
+
+$router->dispatch($_SERVER['QUERY_STRING']);
